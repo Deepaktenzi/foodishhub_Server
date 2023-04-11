5f39594753134f136fab8ed12177be7930243ad7
Deepak Negi <deepaktenzi@gmail.com>
2023-04-11 14:21:39 +0530

    Code Updated

--- app.js
+++ app.js
@@ -2,29 +2,37 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/menu/', (req, res) => {});
-
-app.get('/api/restaurants/', async (req, res) => {
+app.get('/api/restaurants', async (req, res) => {
   const { lat, lng } = req.query;
+  console.log(lat, lng);
   const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
-  const data = await axios.get(url);
-  res.send(data);
+  const resturants = await axios
+    .get(url, {
+      headers: {
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
+      },
+    })
+    .then((res) => res.data);
+
+  res.status(200).json(resturants);
 });
+
+app.get('/api/menu', async (req, res) => {
+  const { id } = req.query;
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.322232&lng=78.085605&restaurantId=${id}`;
+
+  const menu = await axios.get(url, {
+    headers: {
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
+    },
+  });
+  res.status(200).json(menu.data);
+});
+
 app.listen(PORT);
-
-// export const IMG_CDN =
-//   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
-
-// // https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/
-
-// export const FETCH_MENU_URL =
-//   'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.322232&lng=78.085605&restaurantId=';
-// // export const FETCH_MENU_URL =
-// //   'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=30.322232&lng=78.085605&menuId=';
-
-// export const FETCH_RESTAURANTS_URL =
-//   'https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.322232&lng=78.085605&page_type=DESKTOP_WEB_LISTING';
-
-// export const RESTAURANT_CAROUSELS_API =
-//   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/';
